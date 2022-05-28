96716333a0b8e052a454da9bef887928668bb9b5
JulhashTamim <85074094+JulhashTamim@users.noreply.github.com>
2022-05-29 03:45:59 +0600

    re check on server error

--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+
 import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
@@ -7,17 +8,18 @@ import postRoutes from './routes/posts.js';
 
 const app = express();
 
+app.use('/posts', postRoutes);
+
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
 
-app.use('/posts', postRoutes);
 
 const CONNECTION_URL = 'mongodb+srv://julhashtamim:julhashtamim987654@p300.sczrt.mongodb.net/?retryWrites=true&w=majority';
 const PORT = process.env.PORT|| 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: ${PORT}`)))
   .catch((error) => console.log(`${error} did not connect`));
 
 mongoose.set('useFindAndModify', false);
