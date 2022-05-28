52266c88241fcf67515b383bc56732f9137140f5
JulhashTamim <85074094+JulhashTamim@users.noreply.github.com>
2022-05-29 02:16:26 +0600

    routes controller mod

--- server/index.js
+++ server/index.js
@@ -1,20 +1,23 @@
-import Express  from 'express';
+import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 
-const app = Express();
+import postRoutes from './routes/posts.js';
 
-app.use(bodyParser.json({ limit: '30mb', extended: true }));
-app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
+const app = express();
+
+app.use(bodyParser.json({ limit: '30mb', extended: true }))
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
+
+app.use('/posts', postRoutes);
 
 const CONNECTION_URL = 'mongodb+srv://julhashtamim:julhashtamim987654@p300.sczrt.mongodb.net/?retryWrites=true&w=majority';
 const PORT = process.env.PORT|| 5000;
 
-
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
-  .catch((error) => console.log(error.mesasge));
+  .catch((error) => console.log(`${error} did not connect`));
 
 mongoose.set('useFindAndModify', false);
