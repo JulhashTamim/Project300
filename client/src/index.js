81d59e003541d7444458e6eeaf72baf65e7e8956
JulhashTamim <85074094+JulhashTamim@users.noreply.github.com>
2022-06-22 02:16:25 +0600

    6/22

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { configureStore, applyMiddleware, compose } from '@reduxjs/toolkit';
+import thunk from 'redux-thunk';
 
+import { reducers } from './reducers';
 import App from './App';
+import './index.css';
 
+const store = configureStore(reducers, compose(applyMiddleware(thunk)));
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'),
+);
---
+++ client/src/reducers/index.js
@@ -0,0 +1,5 @@
+import { combineReducers } from '@reduxjs/toolkit';
+
+import posts from './posts';
+
+export const reducers = combineReducers({ posts });
